989c2275a2e36b7d9ba8a5a83df0553f119272f3
Joseph Alvizo <alvizoj@uci.edu>
2019-10-11 23:22:24 -0700

    Home page changes
    Added footer css with resizing text

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -4,7 +4,7 @@ import logo from './logo.svg';
 
 export default class HomeScreen extends React.Component {
     componentDidMount(){
-        document.title = "Alvizo's Engravings - Home"
+        document.title = "Alvizo's Engravings"
     }
 
     render() {
@@ -12,13 +12,13 @@ export default class HomeScreen extends React.Component {
             <div className="App">
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to the mind of Joseph</h1>
+                    <h1 className="App-title">Alvizo's Engravings</h1>
                 </header>
                 <p className="App-intro">
                     To get started, edit <code>src/App.js</code> and save to reload.
                 </p>
                 <footer className="Footer">
-                    <p><b>{'\u00A9'} 2019 Joseph Alvizo. All rights reserved.</b></p>
+                    <b>{'\u00A9'} 2019 Joseph Alvizo. All rights reserved.</b>
                 </footer>
             </div>
         );
