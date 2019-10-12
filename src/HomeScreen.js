f7b649c10aff38b725866269b279ba7c3fa71cd7
Joseph Alvizo <alvizoj@uci.edu>
2019-10-12 10:15:22 -0700

    Changed tab logo/shortcut icon to metallic 'AE' text
    Also updated dependencies using 'npm update -g'

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -11,6 +11,7 @@ export default class HomeScreen extends React.Component {
             <div className="App">
                 <header className="Header">
                     <h1 className="Header-title">Alvizo's Engravings</h1>
+                    <p> </p>
                 </header>
                 <p className="App-intro">
                     To get started, edit <code>src/App.js</code> and save to reload.
