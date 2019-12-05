83153350e2bb4202dc4d1857e8190fd3801190ba
Joseph Alvizo <alvizoj@uci.edu>
2019-12-05 13:53:51 -0800

    Added email button
    Known bugs: white space in top left corner on screen resize/shrink

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './HomeScreen.css';
 import './Button.css';
-import {Button} from 'semantic-ui-react';
+import {Button, Segment} from 'semantic-ui-react';
 import picture from './resources/me.png'
+import gmail from './resources/gmail.png'
 
 
 export default class HomeScreen extends React.Component {
@@ -31,24 +32,29 @@ export default class HomeScreen extends React.Component {
                 <header className="Header">
                     <h1 className="HeaderTitleText">Alvizo's Engravings</h1>
                 </header>
-                <p className="BusinessDescription">
-                <p className="HeaderDescText">Metal Engraving Services</p>
-                    DESCRIPTION OF BUSINESS<br></br>
-                    Based in Delano, CA
-                </p>
+
+                <div className="BusinessDescription">
+                    <p className="HeaderDescText">Metal Engraving Services</p>
+                        DESCRIPTION OF BUSINESS<br></br>
+                        Based in Delano, CA
+                </div>
+
                 <div className="GalleryEntry">
-                    <img src={picture}/>
+                    <img src={picture} alt="pic"/>
                     <div >
                         Engraving of AR-15. Price is $50 per 5 square inch engraving.
+                        <br></br>
+                        <Button color='red' onClick={this.toggleImages}>{this.state.buttonText}</Button>
+                        {this.state.show && <ExtraImages/>}
                     </div>
                 </div>
-                <div>
-                    <Button color='red' onClick={this.toggleImages}>{this.state.buttonText}</Button>
-                    <br/> <br/>
-                    {this.state.show && <ExtraImages/>}
 
+                <h2 className="HeaderTitleText">Contact me</h2>
+                <div>
+                    <img src={gmail} className="GmailIcon" alt="gmailIcon"/>
+                    <Button inverted className="CustomBlackButton"><a href="mailto: bruh@gmail.com">Email Me</a></Button>
                 </div>
-                
+
                 <footer className="Footer">
                     <b>{'\u00A9'} 2019 Joseph Alvizo. All rights reserved.</b>
                 </footer>
@@ -57,22 +63,23 @@ export default class HomeScreen extends React.Component {
     }
 }
 
+function PicAndDescr() {
+    return(
+        <div className="GalleryEntry">
+            <img src={picture} alt="pic"/>
+            <div >
+                Engraving of AR-15. Price is $50 per 5 square inch engraving.
+            </div>
+        </div>
+    );
+}
+
 class ExtraImages extends React.Component{
     render(){
         return(
         <div>
-            <div className="GalleryEntry">
-                <img src={picture}/>
-                <div >
-                    Engraving of AR-15. Price is $50 per 5 square inch engraving.
-                </div>
-            </div>
-            <div className="GalleryEntry">
-                <img src={picture}/>
-                <div >
-                    Engraving of AR-15. Price is $50 per 5 square inch engraving.
-                </div>
-            </div> 
+            <PicAndDescr></PicAndDescr>
+            <PicAndDescr></PicAndDescr>
         </div>
         );
     }
