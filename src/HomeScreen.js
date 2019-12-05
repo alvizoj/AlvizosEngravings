eca739ca94857f8e4c312891dc9bac15e5e981a9
Joseph Alvizo <alvizoj@uci.edu>
2019-12-05 09:16:45 -0800

    Added more pictures that fit cleanly for mobile deviced
    Small css organizational changes

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -3,6 +3,7 @@ import './HomeScreen.css';
 import './Button.css';
 import {Button} from 'semantic-ui-react';
 //import Button from 'react-bootstrap/lib/Button';
+import picture from './resources/me.png'
 
 
 export default class HomeScreen extends React.Component {
@@ -15,16 +16,27 @@ export default class HomeScreen extends React.Component {
             <div className="Site">
                 <header className="Header">
                     <h1 className="HeaderTitleText">Alvizo's Engravings</h1>
-                    <p className="HeaderDescText">Metal Engraving Services</p>
                 </header>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
+                <p className="BusinessDescription">
+                    <p className="HeaderDescText">Metal Engraving Services</p>
+                    DESCRIPTION OF BUSINESS<br></br>
+                    Based in Delano, CA
                 </p>
                 <div className="GalleryEntry">
-                    <div className="GalleryEntryItem">
-                        Varius vel pharetra vel turpis nunc eget lorem dolor. Nam at lectus urna duis convallis convallis tellus id. Ullamcorper morbi tincidunt ornare massa eget egestas. Imperdiet dui accumsan sit amet nulla facilisi morbi tempus. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. Urna porttitor rhoncus dolor purus non. Orci nulla pellentesque dignissim enim sit. Erat velit scelerisque in dictum. Eros donec ac odio tempor orci dapibus ultrices in iaculis. Volutpat commodo sed egestas egestas fringilla phasellus faucibus scelerisque. Nibh venenatis cras sed felis eget velit aliquet. Arcu felis bibendum ut tristique. Eleifend mi in nulla posuere sollicitudin aliquam. Duis at tellus at urna. Iaculis nunc sed augue lacus. Semper auctor neque vitae tempus quam pellentesque nec nam aliquam. Purus in mollis nunc sed id semper. At in tellus integer feugiat scelerisque. Cras adipiscing enim eu turpis egestas pretium aenean.
+                    <img src={picture}/>
+                    <div >
+                        Engraving of AR-15. Price is $50 per 5 square inch engraving.
                     </div>
-                    <div className="GalleryEntryItem">
+                </div>
+                <div className="GalleryEntry">
+                    <img src={picture}/>
+                    <div >
+                        Engraving of AR-15. Price is $50 per 5 square inch engraving.
+                    </div>
+                </div>
+                <div className="GalleryEntry">
+                    <img src={picture} />
+                    <div >
                         Engraving of AR-15. Price is $50 per 5 square inch engraving.
                     </div>
                 </div>
@@ -32,7 +44,7 @@ export default class HomeScreen extends React.Component {
                 <footer className="Footer">
                     <b>{'\u00A9'} 2019 Joseph Alvizo. All rights reserved.</b>
                 </footer>
-            </div>
+            </div> /* Site Container */
         );
     }
 }
